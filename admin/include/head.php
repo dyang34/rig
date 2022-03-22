7690f8b9f4d034083d47127c847fa22cf0eaba72
나영욱 <dyang34@naver.com>
2022-03-22 15:13:30 +0900

    favicon 추가.

--- admin/include/head.php
+++ admin/include/head.php
@@ -16,6 +16,9 @@ header("Content-Type:text/html; charset=utf-8");
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <meta name="robots" content="noindex">  <!-- 검색엔진로봇 수집 차단. -->
         
+		<link rel="shortcut icon" href="/images/common/favicon.ico?" />
+        <link rel="apple-touch-icon-precomposed" href="/images/common/apple-icon-57x57.png?"/>
+        
         <link type="text/css" rel="stylesheet" href="/admin/css/table_style.css?t=<?php echo time(); ?>" />
         
         <script type="text/javascript" src="/js/jquery-3.4.1.min.js"></script>
--- include/head.php
+++ include/head.php
@@ -16,6 +16,9 @@ header("Content-Type:text/html; charset=utf-8");
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <meta name="robots" content="noindex">  <!-- 검색엔진로봇 수집 차단. -->
         
+		<link rel="shortcut icon" href="/images/common/favicon.ico?" />
+        <link rel="apple-touch-icon-precomposed" href="/images/common/apple-icon-57x57.png?"/>
+        
         <link type="text/css" rel="stylesheet" href="css/base.css?t=<?php echo time(); ?>" />
         <link type="text/css" rel="stylesheet" href="css/layout.css?t=<?php echo time(); ?>" />
         <link type="text/css" rel="stylesheet" href="css/common_1.css?t=<?php echo time(); ?>" />
